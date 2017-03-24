ea9ffc08fe27927efb91f5fcd5b531c21cee3334
ukitaka <yuki.takahashi.1126@gmail.com>
2017-03-24 15:55:27 +0900

    Add Read / Write

---
+++ ReadWrite.swift
@@ -0,0 +1,21 @@
+//
+//  ReadWrite.swift
+//  FPRealm
+//
+//  Created by ukitaka on 2017/03/24.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Read
+
+public protocol _Read { }
+
+public struct Read: _Read { }
+
+// MARK: - Write
+
+public protocol _Write { }
+
+public struct Write: _Write { }
--- Sources/FPRealm.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FPRealm.swift
-//  waft
-//
-//  Created by ukitaka on {TODAY}.
-//  Copyright © 2017 waft. All rights reserved.
-//
-
-import Foundation
