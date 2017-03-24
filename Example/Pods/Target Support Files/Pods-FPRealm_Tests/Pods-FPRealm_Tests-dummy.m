93e5f541e63247e452b2a44c7fc32e3eb3603243
ukitaka <ukitaka@linecorp.com>
2017-03-24 15:08:24 +0900

    Remove sources

--- Example/Pods/Target Support Files/Pods-FPRealm_Tests/Pods-FPRealm_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FPRealm_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FPRealm_Tests
-@end
--- Example/Pods/Target Support Files/Pods-FPRealm_Tests/Pods-FPRealm_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FPRealm_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FPRealm_TestsVersionString[];
-
