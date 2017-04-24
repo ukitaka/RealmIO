66d64c4c034403d7eb2a3548e23ef4a608352329
ukitaka <yuki.takahashi.1126@gmail.com>
2017-04-24 23:09:08 +0900

    Add `Objects` for test

---
+++ Tests/Objects.swift
@@ -0,0 +1,15 @@
+//
+//  Objects.swift
+//  RealmTxn
+//
+//  Created by Yuki Takahashi on 2017/04/24.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import RealmTxn
+
+class Dog: Object {
+    dynamic var name: String = ""
+}
