9c1570e7cf09a109e1d253f6762c6723311e91c0
ukitaka <yuki.takahashi.1126@gmail.com>
2017-05-22 18:44:42 +0900

    Use `beTrue()` instead of `be(true)`

--- Tests/CompositionSpec.swift
+++ Tests/CompositionSpec.swift
@@ -25,42 +25,42 @@ class CompositionSpec: QuickSpec {
 
         it("should be `write` when compose `write` and `write`.") {
             let txn = writeTxn.flatMap { _ in writeTxn }
-            expect(txn.isWrite).to(be(true))
+            expect(txn.isWrite).to(beTrue())
         }
 
         it("should be `write` when compose `read` and `write`.") {
             let txn = readTxn.flatMap { _ in writeTxn }
-            expect(txn.isWrite).to(be(true))
+            expect(txn.isWrite).to(beTrue())
         }
 
         it("should be `write` when compose `write` and `read`.") {
             let txn = writeTxn.flatMap { _ in readTxn }
-            expect(txn.isWrite).to(be(true))
+            expect(txn.isWrite).to(beTrue())
         }
 
         it("should be `read` when compose `read` and `read`.") {
             let txn = readTxn.flatMap { _ in readTxn }
-            expect(txn.isRead).to(be(true))
+            expect(txn.isRead).to(beTrue())
         }
 
         it("should be `read` when compose `any(read)` and `read`.") {
             let txn = readAnyTxn.flatMap { _ in readTxn }
-            expect(txn.isRead).to(be(true))
+            expect(txn.isRead).to(beTrue())
         }
 
         it("should be `write` when compose `any(read)` and `write`.") {
             let txn = readAnyTxn.flatMap { _ in writeTxn }
-            expect(txn.isWrite).to(be(true))
+            expect(txn.isWrite).to(beTrue())
         }
 
         it("should be `write` when compose `any(write)` and `read`.") {
             let txn = writeAnyTxn.flatMap { _ in readTxn }
-            expect(txn.isWrite).to(be(true))
+            expect(txn.isWrite).to(beTrue())
         }
 
         it("should be `write` when compose `any(write)` and `write`.") {
             let txn = writeAnyTxn.flatMap { _ in writeTxn }
-            expect(txn.isWrite).to(be(true))
+            expect(txn.isWrite).to(beTrue())
         }
     }
 }
