d89a0928e2d0c86ae3b944e111e7c2c0c8a2834e
ukitaka <yuki.takahashi.1126@gmail.com>
2017-06-01 12:22:26 +0900

    Add spec for `add(objects:)`

--- Tests/RealmOperationSpec.swift
+++ Tests/RealmOperationSpec.swift
@@ -25,16 +25,20 @@ class RealmOperationSpec: QuickSpec {
                 }
             }
 
-            it ("works well") {
-                let dog = Dog()
-                dog.name = "Taro"
-                dog.age = 4
-                let addTaro = Realm.IO.add(dog)
-                try! self.realm.run(io: addTaro)
-                let readTaro = RealmRead<Dog>.object(forPrimaryKey: "Taro")
-                let result = try! self.realm.run(io: readTaro)
-                expect(result?.name).to(equal("Taro"))
-                expect(result?.age).to(equal(4))
+            it ("works well to add 1 object") {
+                let addA = Realm.IO.add(Dog.dogA)
+                try! self.realm.run(io: addA)
+                let readA = RealmRead<Dog>.object(forPrimaryKey: "A")
+                let result = try! self.realm.run(io: readA)
+                expect(result?.name).to(equal("A"))
+                expect(result?.age).to(equal(10))
+            }
+
+            it ("works well to add multiple objects") {
+                let addAll = Realm.IO.add(Dog.dogs)
+                let readAll = Realm.IO.objects(Dog.self)
+                let result = try! self.realm.run(io: addAll.flatMap { _ in readAll })
+                expect(result.count).to(equal(4))
             }
         }
 
