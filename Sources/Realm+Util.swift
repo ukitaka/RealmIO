0eed4a8d2de4d85c7a0ece5cf4ba5b78e28896ef
ukitaka <yuki.takahashi.1126@gmail.com>
2018-04-09 16:54:49 +0900

    Update copyright

--- Sources/Realm+Util.swift
+++ Sources/Realm+Util.swift
@@ -3,7 +3,7 @@
 //  RealmIO
 //
 //  Created by Yuki Takahashi on 2017/04/24.
-//  Copyright © 2017年 waft. All rights reserved.
+//  Copyright © 2018 waft. All rights reserved.
 //
 
 import RealmSwift
--- Sources/RealmIO.swift
+++ Sources/RealmIO.swift
@@ -3,7 +3,7 @@
 //  RealmIO
 //
 //  Created by ukitaka on 2017/03/24.
-//  Copyright © 2017年 waft. All rights reserved.
+//  Copyright © 2018 waft. All rights reserved.
 //
 
 import RealmSwift
